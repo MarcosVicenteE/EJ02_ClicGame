e104d5d7282ae3cd93ffe3c70b31dc61baf88c1d
Marcos Vicente Espasandín <tarde@iMac-de-Profesor-3.local>
2015-05-25 19:19:07 +0200

    Initial Commit

---
+++ EJ02_ClicGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EJ02_ClicGame
+//
+//  Created by tarde on 25/5/15.
+//  Copyright (c) 2015 a13marcosve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EJ02_ClicGameTests/EJ02_ClicGameTests.m
@@ -0,0 +1,40 @@
+//
+//  EJ02_ClicGameTests.m
+//  EJ02_ClicGameTests
+//
+//  Created by tarde on 25/5/15.
+//  Copyright (c) 2015 a13marcosve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface EJ02_ClicGameTests : XCTestCase
+
+@end
+
+@implementation EJ02_ClicGameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
