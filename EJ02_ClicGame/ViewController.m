8937bd3d3716dc3a1dcadc1d47e6f35d386dc82b
Marcos Vicente Espasandín <tarde@iMac-de-Profesor-3.local>
2015-05-25 20:18:44 +0200

    añadido botón, cambio de fondo e cambio de texto

--- EJ02_ClicGame/ViewController.m
+++ EJ02_ClicGame/ViewController.m
@@ -9,7 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelPuntuacion;
+@property (weak, nonatomic) IBOutlet UILabel *labelTiempo;
+//prpeperties del juego
+@property NSInteger tiempo;
+@property NSInteger puntuacion;
 
+//properties de audio
 @end
 
 @implementation ViewController
@@ -17,11 +23,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIImage* miFondo = [UIImage imageNamed:@"fondo"];
+    UIColor* miColorDeFondo =  [UIColor colorWithPatternImage:miFondo];
+    [[self view]setBackgroundColor:miColorDeFondo];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)pulsarBoton{
+    _puntuacion ++;
+    [_labelPuntuacion setText:[NSString stringWithFormat:@"PUNTUACION , %ld",_puntuacion]];
 }
 
 @end
